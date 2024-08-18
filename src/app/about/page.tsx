cc771398a99c6dd9fa2e7b028914101a6a720240
Fillip Lampe <fillip@dlampe.com>
2024-08-18 23:05:49 +0200

    finished enough with about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { get_right_now, get_activity_now, get_emoji_now } from './right-now';
-
+import Link from 'next/link';
 
 export default function About() {
   return (
@@ -40,8 +40,9 @@ export default function About() {
 
         <div className="about-page-content">
           <div className="about-page-content-item itemnormal">
+            <h2 className="about-item-title">Current read</h2>
             <p>
-              Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
+              As of making this I'm reading myself throughout the Lord of the rings series
             </p>
           </div>
           <div className="about-page-content-item itemnormal items-center">
@@ -52,14 +53,15 @@ export default function About() {
         </div>
 
         <div className="about-page-content">
-          <div className="about-page-content-item item60 item-gradient">
-            <h2 className="about-item-title">Coding Skills</h2>
-            <p className="about-item-text"> Rust, WebGl, Glsl, Python, JavaScript/Typescript, C#, and more</p>
-            <p className="about-item-text italic">to be continued</p>
+          <div className="about-page-content-item item60">
+            <h2 className="about-item-title">Hobbies</h2>
+            <p className="about-item-text"> When i'm not at uni, working, or coding, I mostly read, boulder, go hiking, play games, 3d model, paint, or watch series/movies</p>
           </div>
           <div className="about-page-content-item item40 items-center">
-            <h2 className="about-item-title">Favorite Language</h2>
-            <img src="/icons/rust-icon.png" alt="Rust" className="about-item-image" loading='lazy'/>
+            <Link href="https://open.spotify.com/artist/21TinSsF5ytwsfdyz5VSVS?si=C17RMFQARxuzxgPIs0oHOQ">
+              <img src="/icons/dodie-icon.jpeg" alt="Current top artist" className="about-item-image-spotify" loading='lazy'/>
+            </Link>
+            <h2 >Current Top Artist: Dodie</h2>
           </div>
         </div>
 
