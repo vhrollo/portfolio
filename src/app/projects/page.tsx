d50ccd808b37155fd0c9db40db595d432c7d882e
Fillip Lampe <fillip@dlampe.com>
2024-11-18 13:53:52 +0100

    update info

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,19 +10,19 @@ export default function Projects() {
   return (
     <main className="flex min-h-screen flex-col items-center px-6 max-w-screen-lg mx-auto">
       <h1 className="about-page-title">Projects Page</h1>
-      <h2 className="about-page-subtitle">Some of my side projects I&apos;ve been cooking up</h2>
+      <h2 className="about-page-subtitle">Some of my side projects I&apos;ve been cooking up outside uni</h2>
       <div className="card-grid">
         <Card
           title="Water Simulation"
           imageSrc={water}
           link="https://github.com/vhrollo/fluid_simulations"
-          description="A simple 2D Smoothed Particle Hydrodynamics (SPH) simulation using WGSL, and some GLSL (as I switched mid-project), and Rust."
+          description="A GPU-accelerated 2D Smoothed Particle Hydrodynamics (SPH) simulation using WGSL, and some GLSL (as I switched mid-project), and Rust. For the GPU part, compute shaders was used."
         />
         <Card
           title="This website"
           imageSrc={portfolio}
           link="https://github.com/vhrollo/portfolio"
-          description="A simple website made in react, nextjs, and tailwindcss(, but mostly normal css)."
+          description="This website made in react, nextjs, and tailwindcss(, but mostly normal css)."
         />       
       </div>
     </main>
