7dbacb9332dfac76a3fb2380379e8b73440f4e2d
vhrollo <103120448+vhrollo@users.noreply.github.com>
2025-06-16 13:04:49 +0200

    new fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({ children,}: Readonly<{children: React.React
         <Script
           src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
           strategy="afterInteractive"
-          crossOrigin="anonymous"
         />
       </body>
     </html>
